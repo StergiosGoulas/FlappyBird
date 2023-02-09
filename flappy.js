596427ecb8d27ee15957db3c28519f09fc74e2f1
StergiosGoulas <57486520+StergiosGoulas@users.noreply.github.com>
2023-02-09 12:04:35 +0000

    finish

--- flappy.js
+++ flappy.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-const birdImage = new Image();
-birdImage.src = "../images/bird.png";
+const birdSprites = new Image();
+birdSprites.src = "../images/bird.png";
 
 let upPipe = new Image();
 upPipe.src = "../images/pipesUp.png";
@@ -19,66 +19,62 @@ const bird = {
   width: 49,
   height: 50,
   grav: 1,
-  jump: -10
+  jump: -10,
+  currentFrame: 0
 };
 
 const pipes = [];
 let score = 0;
 
 function update() {
-    bird.y += bird.grav;
-    bird.grav += 0.5;
-  
-    if (bird.y > canvas.height - bird.height) {
-      bird.y = canvas.height - bird.height;
-      bird.grav = 0;
-    }
-  
-    if (bird.y < 0) {
-      bird.y = 0;
-      bird.grav = 0;
-    }
-  
-    if (pipes.length === 0 || pipes[pipes.length - 1].x < canvas.width - 100) {
-        pipes.push({
-          x: canvas.width + 400,
-          y: Math.floor(Math.random() * (canvas.height - 200)) + 50,
-          width: 51,
-          height: 200
-        });
-      }
-      
-  
-    for (let i = 0; i < pipes.length; i++) {
-      pipes[i].x -= 2;
-  
-      if (pipes[i].x + pipes[i].width < 0) {
-        pipes.shift();
-        score++;
-      }
-  
-      if (
-        (bird.x + bird.width >= pipes[i].x &&
-          bird.x <= pipes[i].x + pipes[i].width) &&
-        (bird.y <= pipes[i].y || bird.y + bird.height >= pipes[i].y + pipes[i].height)
-      ) {
-        document.location.reload();
-      }
-    }
-  }
-  
+  bird.currentFrame = (bird.currentFrame + 1) % 3;  //allagh eikonas ka8e 3 frames
+  bird.y += bird.grav;
+  bird.grav += 0.5;
 
+  if (bird.y > canvas.height - bird.height) {
+    bird.y = canvas.height - bird.height;
+    bird.grav = 0;
+  }
+
+  if (bird.y < 0) {
+    bird.y = 0;
+    bird.grav = 0;
+  }
+
+  if (pipes.length === 0 || pipes[pipes.length - 1].x < canvas.width - 100) {
+    pipes.push({
+      x: canvas.width + 400,
+      y: Math.floor(Math.random() * (canvas.height - 200)) + 50,
+      width: 51,
+      height: 200  //to keno anamesa sto upPipe kai to downPipe
+    });
+  }
+
+  for (let i = 0; i < pipes.length; i++) {
+    pipes[i].x -= 2;
+
+    if (pipes[i].x + pipes[i].width < 0) {
+      pipes.shift();
+      score++;
+    }
+
+    if ((bird.x + bird.width >= pipes[i].x && bird.x <= pipes[i].x + pipes[i].width) && (bird.y <= pipes[i].y || bird.y + bird.height >= pipes[i].y + pipes[i].height)) {
+      document.location.reload();
+    }
+
+  }
+}
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   ctx.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
 
-  ctx.drawImage(birdImage, bird.x, bird.y, bird.width, bird.height);
+  ctx.drawImage(birdSprites, bird.currentFrame * bird.width, 0, bird.width, bird.height,bird.x, bird.y, bird.width, bird.height);
 
   for (let i = 0; i < pipes.length; i++) {
-    ctx.drawImage(downPipe, pipes[i].x, 0, pipes[i].width, pipes[i].y);
     ctx.drawImage(upPipe, pipes[i].x, pipes[i].y + pipes[i].height, pipes[i].width, canvas.height);
+    ctx.drawImage(downPipe, pipes[i].x, 0, pipes[i].width, pipes[i].y);
   }
 
   ctx.fillStyle = "black";
@@ -91,8 +87,8 @@ setInterval(() => {
   draw();
 }, 16);
 
-document.onkeydown = function(e) {
-    if (e.code === "ArrowUp") {
-      bird.grav = bird.jump;
-    }
-  };
+document.onkeydown = function (e) {
+  if (e.code === "ArrowUp") {
+    bird.grav = bird.jump;
+  }
+};
